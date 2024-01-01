2818c03a087345488baa1a06719b37882b6d01bf
Yoshitaka Kato <ykato449876@gmail.com>
2024-01-01 22:04:03 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace findZipCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxPref = new System.Windows.Forms.ListBox();
+            this.lbxCity = new System.Windows.Forms.ListBox();
+            this.lbxStreet = new System.Windows.Forms.ListBox();
+            this.lblPref = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblStreet = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxPref
+            // 
+            this.lbxPref.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbxPref.FormattingEnabled = true;
+            this.lbxPref.ItemHeight = 33;
+            this.lbxPref.Location = new System.Drawing.Point(133, 40);
+            this.lbxPref.Name = "lbxPref";
+            this.lbxPref.Size = new System.Drawing.Size(207, 763);
+            this.lbxPref.TabIndex = 0;
+            this.lbxPref.SelectedIndexChanged += new System.EventHandler(this.lbxPref_SelectedIndexChanged);
+            // 
+            // lbxCity
+            // 
+            this.lbxCity.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbxCity.FormattingEnabled = true;
+            this.lbxCity.ItemHeight = 33;
+            this.lbxCity.Location = new System.Drawing.Point(454, 40);
+            this.lbxCity.Name = "lbxCity";
+            this.lbxCity.Size = new System.Drawing.Size(302, 763);
+            this.lbxCity.TabIndex = 1;
+            this.lbxCity.SelectedIndexChanged += new System.EventHandler(this.lbxCity_SelectedIndexChanged);
+            // 
+            // lbxStreet
+            // 
+            this.lbxStreet.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbxStreet.FormattingEnabled = true;
+            this.lbxStreet.ItemHeight = 33;
+            this.lbxStreet.Location = new System.Drawing.Point(874, 40);
+            this.lbxStreet.Name = "lbxStreet";
+            this.lbxStreet.Size = new System.Drawing.Size(645, 763);
+            this.lbxStreet.TabIndex = 2;
+            this.lbxStreet.SelectedIndexChanged += new System.EventHandler(this.lbxStreet_SelectedIndexChanged);
+            // 
+            // lblPref
+            // 
+            this.lblPref.AutoSize = true;
+            this.lblPref.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblPref.Location = new System.Drawing.Point(51, 40);
+            this.lblPref.Name = "lblPref";
+            this.lblPref.Size = new System.Drawing.Size(47, 33);
+            this.lblPref.TabIndex = 3;
+            this.lblPref.Text = "県";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblCity.Location = new System.Drawing.Point(383, 40);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(47, 33);
+            this.lblCity.TabIndex = 4;
+            this.lblCity.Text = "市";
+            // 
+            // lblStreet
+            // 
+            this.lblStreet.AutoSize = true;
+            this.lblStreet.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblStreet.Location = new System.Drawing.Point(801, 40);
+            this.lblStreet.Name = "lblStreet";
+            this.lblStreet.Size = new System.Drawing.Size(47, 33);
+            this.lblStreet.TabIndex = 5;
+            this.lblStreet.Text = "町";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblPostalCode.Location = new System.Drawing.Point(257, 824);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(0, 40);
+            this.lblPostalCode.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1974, 929);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblStreet);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblPref);
+            this.Controls.Add(this.lbxStreet);
+            this.Controls.Add(this.lbxCity);
+            this.Controls.Add(this.lbxPref);
+            this.Name = "Form1";
+            this.Text = "郵便番号検索";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxPref;
+        private System.Windows.Forms.ListBox lbxCity;
+        private System.Windows.Forms.ListBox lbxStreet;
+        private System.Windows.Forms.Label lblPref;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblStreet;
+        private System.Windows.Forms.Label lblPostalCode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace findZipCode
+{
+    public partial class Form1 : Form
+    {
+        private const string magicWord = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=";
+        private const string magicTail = ";Extended Properties='Excel 12.0; HDR=Yes'";
+        public string excelFile;
+        public string city;
+        public string pref;
+        public string street;
+         public Form1()
+        {
+            InitializeComponent();
+            excelFile = get_excel_file("findZipCode.ini");
+            get_pref_list();
+            myinit();
+        }
+        private string get_excel_file(string filename)
+        {
+            string excelFileName = null;
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    string[] lines = File.ReadAllLines(filename);
+                    foreach (var line in lines)
+                    {
+                        // 行が # で始まっていないか確認
+                        if (!line.StartsWith("#"))
+                        {
+                            // EXCELFILE> が含まれている行を探す
+                            if (line.StartsWith("EXCELFILE>"))
+                            {
+                                // 文字列から不要な部分を除去して、excelFileName に割り当てる
+                                excelFileName = line.Replace("EXCELFILE>", "").Trim();
+                                break;  // 見つけたらループを終了
+                            }
+                        }
+                    }
+                    if (string.IsNullOrEmpty(excelFileName))
+                    {
+                        MessageBox.Show("Error !! content of your " + filename + " is invalid. ");
+
+                    }
+                }
+
+            } catch {
+                MessageBox.Show("Error!!");
+            }
+            return excelFileName;
+
+        }
+        private void myinit()
+        {
+            lbxCity.SelectionMode = SelectionMode.One;
+            lbxPref.SelectionMode = SelectionMode.One;
+            lbxStreet.SelectionMode = SelectionMode.One;
+            lblPostalCode.Text = string.Empty;
+            this.Width = 800;
+        }
+        public void get_pref_list()
+        {
+            String connectionString = magicWord + excelFile + magicTail;
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                string query = "SELECT prefNo, pref from [pref$] order by prefNo;";
+                OleDbCommand comm = new OleDbCommand(query, conn);
+                conn.Open();
+                using (var dr = comm.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        this.lbxPref.Items.Add(Convert.ToString(dr["pref"]));
+                    }
+                }
+            }
+        }
+
+        private void lbxPref_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String connectionString = magicWord + excelFile + magicTail;
+            this.lbxCity.Items.Clear();
+            pref = Convert.ToString(lbxPref.SelectedItem);
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                string query = "SELECT DISTINCT city from [zipcode$] where pref = \"" + pref + "\";";
+                OleDbCommand comm = new OleDbCommand(query, conn);
+                conn.Open();
+                using (var dr = comm.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        this.lbxCity.Items.Add(Convert.ToString(dr["city"]));
+                    }
+                }
+            }
+        }
+
+        private void lbxCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String connectionString = magicWord + excelFile + magicTail;
+            this.lbxStreet.Items.Clear();
+            city = Convert.ToString(lbxCity.SelectedItem);
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                string query = "SELECT DISTINCT street from [zipcode$] where pref = \""+
+                    pref + "\" AND city = \"" + city + "\";";
+                OleDbCommand comm = new OleDbCommand(query, conn);
+                conn.Open();
+                using (var dr = comm.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        street = Convert.ToString(dr["street"]);
+                        this.lbxStreet.Items.Add(street);
+                    }
+                }
+            }
+
+        }
+
+        private void lbxStreet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String connectionString = magicWord + excelFile + magicTail;
+            street = Convert.ToString(lbxStreet.SelectedItem);
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                string query = "SELECT zipcode from [zipcode$] where pref = \"" + 
+                    pref+ "\" AND city = \"" + city + 
+                    "\" AND street = \"" + street + "\";";
+                OleDbCommand comm = new OleDbCommand(query, conn);
+                conn.Open();
+                using (var dr = comm.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        string postalNumber = Convert.ToString(dr["zipcode"]);
+                        string postalCode = postalNumber.Insert(3, "-");
+                        this.lblPostalCode.Text = pref+city+street+ "の郵便番号は " + postalCode;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
